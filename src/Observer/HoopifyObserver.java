721fed5022efc892b387e7f49e571f67a27af90c
kunusa <221732@astanait.edu.kz>
2023-11-18 18:42:17 +0100

    observer implementation

---
+++ src/Observer/HoopifyObserver.java
@@ -0,0 +1,7 @@
+
+
+package Observer;
+
+public interface HoopifyObserver {
+    void update();
+}
---
+++ src/Observer/HoopifySubject.java
@@ -0,0 +1,10 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package Observer;
+
+public interface HoopifySubject {
+    void addObserver(HoopifyObserver var1);
+}
